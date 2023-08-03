320c457e447c42f6f3e7335f4b41727aeb6a4420
Lucas Lessa <lessa07oliveira@gmail.com>
2023-08-03 11:41:07 -0300

    l

--- script.js
+++ script.js
@@ -1,114 +1,137 @@
-//Algoritmo 
-
-//Calcular Idade
-//1.Pegar os valores
-//2.Calcular a idade
-//3.Gerar a classificaçao da idade
-//4.Organizar as informaçoes
-//5.Salvar os dados na lista
-
-
-
-
-
 function calcularIdade(event) {
-    event.preventDefault(); // Impede que a página seja recarregada
+    event.preventDefault()
 
-    let dadosUsuarios = pegarValores();
-    let idade = calcular(dadosUsuarios.ano);
-    let classificacao = classificarIdade(idade); // Chama a função e passa a idade calculada como argumento
+    let dadosUsuario = pegarValores();
 
-    console.log(classificacao);
-    let usuarioAtualizado = organizarDados(dadosUsuarios, idade, classificacao);
+    let idade = calcular(dadosUsuario.diaNascimento, dadosUsuario.mesNascimento, dadosUsuario.anoNascimento);
 
-    cadastrarUsuario(usuarioAtualizado);
+    let classificacaoIdade = classificarIdade(idade.idadeAnos);
+
+    let usuarioAtt = organizarDados(dadosUsuario, idade, classificacaoIdade);
+
+    cadastrarUsuario(usuarioAtt);
+
+    window.location.reload();
 }
 
-//Passo 1.Pegar os valores
 function pegarValores() {
-
-    let nomeRecebido = document.getElementById("nome").value.trim();
+    let nome = document.getElementById("nome").value.trim();
     let diaNascimento = parseInt(document.getElementById("dia-nascimento").value);
-    let mesUsuario = parseInt(document.getElementById("mes-nascimento").value);
-    let anoUsuario = parseInt(document.getElementById("ano-nascimento").value);
-    
+    let mesNascimento = parseInt(document.getElementById("mes-nascimento").value);
+    let anoNascimento = parseInt(document.getElementById("ano-nascimento").value);
 
     let dadosUsuario = {
-        nome : nomeRecebido,
-        dia : diaNascimento ,
-        mes : mesUsuario,
-        ano : anoUsuario 
-    }
+        nome: nome,
+        diaNascimento: diaNascimento,
+        mesNascimento: mesNascimento,
+        anoNascimento: anoNascimento
 
-    console.log(dadosUsuario);
+    }
 
     return dadosUsuario;
 }
 
-//Passo 2.Calcular
-function calcular(ano) {
-    let anoAtual = new Date();
+function calcular(diaNascimento, mesNascimento, anoNascimento) {
+    let dataAtual = new Date();
+    let diaAtual = dataAtual.getDate();
+    let mesAtual = dataAtual.getMonth() + 1;
+    let anoAtual = dataAtual.getFullYear();
 
-    let idadeAtual = anoAtual.getFullYear()- ano;
+    let idadeAnos = anoAtual - anoNascimento;
+    let idadeMeses = mesAtual - mesNascimento;
+    let idadeDias = diaAtual - diaNascimento;
 
-    console.log(idadeAtual);
-
-    return idadeAtual;
-}
-
-//Passo 3.Classificar Idade
-
-function classificarIdade(idadeAtual) {
-    /*  Resultado            Faixa
-    0 à 12                Criança
-    13 à 17                Adolescente
-    18 à 65               Adulto
-    Acima de 65         Idoso*/
-
-    if (idadeAtual <= 12) {
-        return "Criança";
-    } else if (idadeAtual <= 17) {
-        return "Adolescente"
-    } else if (idadeAtual <= 65) {
-        return "Adulto"
-    } else {
-        return "Idoso"
+    if (idadeDias < 0) {
+        idadeMeses--;
+        idadeDias += 30;
     }
 
-
-}
-
-//Passo 4. Organizar Informacoes
-
-function organizarDados(dadosUsuario, idade, classificacao) {
-    let dataAtual = new Date();
-    let dataNascimento = new Date(dadosUsuario.ano, dadosUsuario.mes - 1, dadosUsuario.dia);
-
-    // Calcula a idade em anos, meses e dias
-    let idadeAnos = dataAtual.getFullYear() - dataNascimento.getFullYear();
-    let idadeMeses = dataAtual.getMonth() - dataNascimento.getMonth();
-    let idadeDias = dataAtual.getDate() - dataNascimento.getDate();
-
-    // Ajuste para verificar se ainda não completou todos os meses do ano atual
-    if (idadeMeses < 0 || (idadeMeses === 0 && idadeDias < 0)) {
+    if (idadeMeses < 0) {
         idadeAnos--;
         idadeMeses += 12;
     }
 
+    return { idadeAnos, idadeMeses, idadeDias };
+}
+
+function classificarIdade(idadeAnos) {
+    if (idadeAnos >= 0 && idadeAnos <= 12) {
+        return "Criança"
+
+    } else if (idadeAnos >= 13 && idadeAnos <= 17) {
+        return "Adolescente"
+
+    } else if (idadeAnos >= 18 && idadeAnos <= 65) {
+        return "Adulto"
+
+    } else {
+        return "Idoso"
+    }
+}
+
+function organizarDados(dadosUsuario, valorIdade, classificacaoIdade) {
     let dadosUsuarioAtt = {
         ...dadosUsuario,
-        idadeAnos: idadeAnos,
-        idadeMeses: idadeMeses,
-        idadeDias: idadeDias,
-        classificacao: classificacao,
+        idadeAnos: valorIdade.idadeAnos,
+        idadeMeses: valorIdade.idadeMeses,
+        idadeDias: valorIdade.idadeDias,
+        classificacao: classificacaoIdade,
     };
 
     return dadosUsuarioAtt;
 }
 
+function cadastrarUsuario(usuario) {
+    let listaUsuarios = [];
 
+    // if (localStorage.getItem("usuariosCadastrados") == true) padrao do if
+    if (localStorage.getItem("usuariosCadastrados")) {
+        listaUsuarios = JSON.parse(localStorage.getItem("usuariosCadastrados"))
+    }
 
+    listaUsuarios.push(usuario)
 
+    localStorage.setItem("usuariosCadastrados", JSON.stringify(listaUsuarios))
+}
 
+function carregarUsuarios() {
+    let listaUsuarios = [];
 
+    if (localStorage.getItem("usuariosCadastrados")) {
+        listaUsuarios = JSON.parse(localStorage.getItem("usuariosCadastrados"));
+    }
 
+    if (listaUsuarios.length == 0) {
+        let tabela = document.getElementById("corpo-tabela");
+
+        tabela.innerHTML = `<tr class="linha-mensagem">
+            <td colspan="6">Nenhum usuário cadastrado !</td>
+        </tr>`
+    } else {
+        montarTabela(listaUsuarios);
+    }
+}
+
+window.addEventListener('DOMContentLoaded', () => carregarUsuarios());
+
+function montarTabela(listaDeCadastrados) {
+    let tabela = document.getElementById("corpo-tabela");
+
+    let template = '';
+
+    listaDeCadastrados.forEach(pessoa => {
+        template += ` <tr>
+        <td data-cell="nome">${pessoa.nome}</td>
+        <td data-cell="data de nascimento">${pessoa.diaNascimento}/${pessoa.mesNascimento}/${pessoa.anoNascimento}</td>
+        <td data-cell="idade">${pessoa.idadeAnos}Ano(s), ${pessoa.idadeMeses}Mes(es) e ${pessoa.idadeDias}Dia(s).</td>
+        <td data-cell="faixa etária">${pessoa.classificacao}</td>
+    </tr>`
+    });
+
+    tabela.innerHTML = template;
+}
+
+function deletarRegistros() {
+    localStorage.removeItem("usuariosCadastrados")
+    window.location.reload();
+}
